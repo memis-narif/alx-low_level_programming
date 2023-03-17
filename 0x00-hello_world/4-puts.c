8c364713dac5a000e9b7d3b03645e80d513c114d
memis-narif <anirbellahcen19@outlook.com>
2023-03-17 12:50:15 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
 * main - A program that prints a line with puts function
+* main: return value of the function
 * C code to show using return statement
 * return: 0 (success)
 */
